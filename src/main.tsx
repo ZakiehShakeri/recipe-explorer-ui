c37d05afdf1d30d574c18f1cfdca5776fd20baeb
Zakieh Shakeri <zakieh.z@gmail.com>
2025-04-24 17:25:02 +0330

    refactor: move recipe-explorer to Vite template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,141 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { MagnifyingGlassIcon, PaperPlaneIcon } from "@radix-ui/react-icons";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { Box, Card, Flex, Heading, IconButton, Inset, Strong, Text, TextField, Theme } from "@radix-ui/themes";
+import { Link } from "react-router-dom";
+import { useState, useEffect} from 'react';
+
+type SampleFoodProps = { imageUrl: string, foodName: string, description: string };
+
+function SampleFood(sampleFood: SampleFoodProps) {
+    return (<Box maxWidth={"300px"} height={"250px"}>
+        <Card size="2">
+            <Inset clip="padding-box" side="top" pb="current">
+                {sampleFood.imageUrl !== "" &&
+                    <img
+                        src={sampleFood.imageUrl}
+                        alt="Bold typography"
+                        style={{
+                            display: "block",
+                            objectFit: "cover",
+                            width: "100%",
+                            height: 140,
+                            backgroundColor: "var(--gray-5)",
+                        }}
+                        crossOrigin="anonymous"
+                    />}
+            </Inset>
+            <Text as="p" size="3">
+                <Strong><Link to={`/recipe?foodName=${encodeURIComponent(sampleFood.foodName)}`}>{sampleFood.foodName}</Link></Strong> {sampleFood.description}
+            </Text>
+        </Card>
+    </Box>);
+}
+
+function SampleFoodRow({ sf }: { sf: SampleFoodProps[] }) {
+    return (<Flex justify={"between"} align={"center"} gap={"8"} >
+        {sf.map(n => <SampleFood key={n.foodName} imageUrl={n.imageUrl} foodName={n.foodName} description={n.description} />)}
+    </Flex>)
+}
+
+function Root() {
+    const [sampleFoods, setSampleFoods] = useState([
+        { foodName: "Fesenjoon", description: "A beloved Persian dish, celebrated for its sweet, tangy, and nutty flavors.", imageUrl: "images/fesenjoon.jpeg" },
+        { foodName: "Ghormeh Sabzi", description: "A Persian herb stew with kidney beans and dried limes.", imageUrl: "images/ghormeh sabzi.jpeg" },
+        { foodName: "Kebab", description: "A delicious Persian dish made with ground meat, onions, and spices.", imageUrl: "images/kebab.jpeg" },
+        { foodName: "Zereshk Polo", description: "A Persian rice dish with barberries, saffron, and pistachios.", imageUrl: "images/zereshk polo.jpeg" },
+        { foodName: "Abgoosht", description: "A traditional Persian soup made with lamb, chickpeas, and potatoes.", imageUrl: "images/abgoosht.jpeg" },
+        { foodName: "Ghalieh Mahi", description: "A spicy Persian fish stew made with herbs, garlic, and tamarind.", imageUrl: "images/ghalieh mahi.jpeg" },
+    ]);
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            const updatedFoods = await Promise.all(sampleFoods.map(async (food) => {
+                if (food.imageUrl && food.imageUrl !== "")
+                    return food;
+                // Fetch the image URL from the API
+                const response = await fetch(`https://recipe-explorer-api.zakieh-z.workers.dev/getImage?foodOrIngName=${food.foodName}`, { mode: "cors" });
+                const imageJson = await response.json();
+                if (!imageJson.thumb)
+                    return { ...food, imageUrl: "images/general.jpg" };
+                return { ...food, imageUrl: imageJson.thumb };
+            }));
+            setSampleFoods(updatedFoods);
+        };
+
+        fetchImages();
+    }, []);
+
+    return (
+        <Flex direction={"column"} justify={"center"} align={"center"}>
+            <Theme accentColor="blue" appearance="light">
+                <Flex direction={"column"} gap={"4"} justify={"center"} align={"stretch"} maxWidth="1000px">
+                    {/* Header */}
+                    <Flex direction={"row"} justify={"center"}>
+                        <Flex justify="center" align="center" p="4"><Heading size={"9"} wrap={"pretty"} color="gray">Recipe Explorer</Heading></Flex>
+                        {/* <img
+                            src="images/fesenjoon2.jpg"
+                            style={{
+                                display: "block",
+                                objectFit: "cover",
+                                flexBasis: "2000px",
+                                flexGrow: 1,
+                                flexShrink: 1,
+                                backgroundColor: "var(--gray-5)",
+                                height: 250
+                            }
+
+                            }
+                        /> */}
+                    </Flex>
+
+                    {/* Search Bar */}
+                    <Card size={"2"}>
+                        <Flex direction={"column"} gap={"4"} align={"stretch"} justify={"center"}>
+
+                            <Flex direction={"column"} gap={"4"} align={"start"} justify={"start"} flexGrow={"1"} flexBasis={"200px"}>
+                                <Box>
+                                    <Text as="p" size="4" color="gray" >
+                                        Hi! I'm your cooking assistant. Ask me about new recipes you want to explore
+
+                                        or any other questions you may have 😊.
+                                    </Text>
+                                    <br />
+                                </Box>
+
+                            </Flex>
+                            <Box>
+                                <form onSubmit={(e) => {
+                                    e.preventDefault();
+                                    const question = (e.target as HTMLFormElement).question.value;
+                                    if (question) {
+                                        window.location.replace(`?p=recipe&foodName=${encodeURIComponent(question)}`);
+
+                                    }
+                                }}>
+                                    <TextField.Root size={"3"} name="question" placeholder="Ask your questions here…">
+                                        <TextField.Slot>
+                                            <MagnifyingGlassIcon height="16" width="16" />
+                                        </TextField.Slot>
+                                        <TextField.Slot>
+                                            <IconButton type="submit" size="1" variant="ghost">
+                                                <PaperPlaneIcon height="14" width="14" />
+                                            </IconButton>
+                                        </TextField.Slot>
+                                    </TextField.Root>
+                                </form>
+                            </Box>
+
+                        </Flex>
+                    </Card>
+                    {/* Sample Foods */}
+                    {[0].map(i => <SampleFoodRow key={`${i}`} sf={sampleFoods.slice(0, 3)} />)}
+                    {[1].map(i => <SampleFoodRow key={`${i}`} sf={sampleFoods.slice(3, 6)} />)}
+                </Flex>
+            </Theme>
+        </Flex>
+    );
+}
+
+export default function Main() {
+    return <Root />
+}
---
+++ src/recipe.tsx
@@ -0,0 +1,195 @@
+import { MagnifyingGlassIcon, PaperPlaneIcon } from "@radix-ui/react-icons";
+import { Box, Card, Flex, Heading, HoverCard, IconButton, Inset, Link, Spinner, Strong, Text, TextField, Theme } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
+
+type Ingredient = { name: string, amount: string, description: string };
+type RecipeProps = { name: string, description: string, ingredients: Ingredient[], instructions: string[] };
+
+function useImageUrl(foodOrIngName: string) {
+    const [imageUrl, setImageUrl] = useState<string>('');
+
+    useEffect(() => {
+        const fetchImageUrl = async () => {
+            if (!foodOrIngName) return;
+            try {
+                const response = await fetch(`https://recipe-explorer-api.zakieh-z.workers.dev/getImage?foodOrIngName=${foodOrIngName}`, { mode: "cors" });
+                if (!response.ok) {
+                    throw new Error('Failed to fetch image');
+                }
+                const data = await response.json();
+                if (!data.thumb)
+                    setImageUrl("images/general.jpg");
+                setImageUrl(data.url);
+            } catch (error) {
+                console.error('Error fetching image:', error);
+            }
+        };
+
+        fetchImageUrl();
+    }, [foodOrIngName]);
+
+    return imageUrl;
+}
+
+function IngredientHoverCard({ ing }: { ing: Ingredient }) {
+    const imgUrl = useImageUrl(ing.name);
+    return <Flex>
+        <Box asChild flexShrink="0">
+            <Inset side="left" pr="current">
+                <img
+                    src={imgUrl}
+                    alt={ing.name}
+                    style={{
+                        display: "block",
+                        objectFit: "cover",
+                        height: "100%",
+                        width: 150,
+                        backgroundColor: "var(--gray-5)",
+                    }}
+                />
+            </Inset>
+        </Box>
+        <Text size="2" as="p">
+            <Strong>{ing.name}</Strong>{ing.description}
+        </Text>
+    </Flex>
+
+}
+
+function ShowIngredient({ ing }: { ing: Ingredient }) {
+
+    return (
+        <HoverCard.Root>
+            <HoverCard.Trigger>
+                <Link href="#">{ing.name}</Link>
+            </HoverCard.Trigger>
+
+            <HoverCard.Content width="450px">
+                <IngredientHoverCard ing={ing} />
+            </HoverCard.Content>
+        </HoverCard.Root>
+    );
+}
+
+
+
+export default function Recipe() {
+    const searchParams = new URLSearchParams(window.location.search);
+    const foodName = searchParams.get('foodName');
+    const [recipe, setRecipe] = useState<RecipeProps | null>(null);
+    const imgUrl = useImageUrl(recipe?.name ?? '');
+
+    useEffect(() => {
+        const fetchRecipe = async function () {
+            if (!foodName) return;
+            try {
+                const response = await fetch(`https://recipe-explorer-api.zakieh-z.workers.dev/getRecipe?foodName=${foodName}`, { mode: "cors" });
+                if (!response.ok) {
+                    throw new Error('Failed to fetch recipe');
+                }
+                const data = await response.json();
+                setRecipe(data);
+            } catch (error) {
+                console.error('Error fetching recipe:', error);
+            }
+        }
+
+        fetchRecipe();
+    }, [foodName]); // Add foodName as dependency
+
+
+
+
+    return (
+        <Flex direction={"column"} justify={"center"} align={"center"}>
+            <Theme accentColor="blue" appearance="light">
+                <Flex direction={"column"} gap={"4"} justify={"center"} align={"stretch"} maxWidth="1000px" >
+                    {!recipe && (
+                        <Flex direction="column" justify="center" align="center" height="100vh" gap={"4"}>
+                            <Spinner />
+                            <Text size="3" mt="4">Loading recipe...</Text>
+                        </Flex>
+                    )}
+                    {/* Header */}
+                    <Flex direction={"row"} align={"stretch"} justify={"between"} maxHeight={"150px"} >
+                        <Flex justify="center" align="center" p="4">
+                            <Heading size={"9"} wrap={"pretty"} color="gray">
+                                {recipe?.name}
+                            </Heading>
+                        </Flex>
+                        {imgUrl && (
+                            <img
+                                src={imgUrl}
+                                style={{
+                                    display: "block",
+                                    objectFit: "cover",
+                                    backgroundColor: "var(--gray-5)",
+                                    maxWidth: "600px"
+                                }}
+                            />
+                        )}
+                    </Flex>
+
+                    {/* Ingredients */}
+                    <Card size={"2"}>
+                        <Flex direction={"column"} gap={"4"} align={"start"} justify={"start"} >
+                            <Heading size={"4"} wrap={"pretty"} color="gray">Ingredients</Heading>
+                            {recipe?.ingredients.length && (
+                                <ul>
+                                    {recipe.ingredients.map((ingredient, i) => (
+                                        <li key={`${i}`}>{ingredient.amount} <ShowIngredient ing={ingredient} /></li>
+                                    ))}
+                                </ul>
+                            )}
+                        </Flex>
+                    </Card>
+
+                    {/* Instructions */}
+                    <Text>
+                        <Heading size={"4"} wrap={"pretty"} color="gray">Instructions</Heading>
+                        <ul>
+                            {recipe?.instructions.map((instruction, i) => (
+                                <li key={`${i}`}>{instruction}</li>
+                            ))}
+                        </ul>
+                        {/* Fallback for instructions if not an array */}
+                        {Array.isArray(recipe?.instructions) ? null : (
+                            <Text size="2" color="gray">
+                                {recipe?.instructions}
+                            </Text>
+                        )}
+
+                    </Text>
+
+                    {/* Search Bar */}
+                    <Card size={"2"}>
+                        <Flex direction={"column"} gap={"4"} align={"stretch"} justify={"center"}>
+
+                            <Flex direction={"column"} gap={"4"} align={"start"} justify={"start"} flexGrow={"1"} flexBasis={"100px"}>
+                                <Box>
+                                    <Text as="p" size="4" color="gray" >
+                                        Hi! I'm your cooking assistant. Ask me any question you may have from this recipe 😊.
+                                    </Text>
+                                    <br />
+                                </Box>
+
+                            </Flex>
+                            <TextField.Root size={"3"} placeholder="Ask your questions here…">
+                                <TextField.Slot>
+                                    <MagnifyingGlassIcon height="16" width="16" />
+                                </TextField.Slot>
+                                <TextField.Slot>
+                                    <IconButton size="1" variant="ghost">
+                                        <PaperPlaneIcon height="14" width="14" />
+                                    </IconButton>
+                                </TextField.Slot>
+                            </TextField.Root>
+
+                        </Flex>
+                    </Card>
+
+                </Flex>
+            </Theme>
+        </Flex>
+    );
+}
